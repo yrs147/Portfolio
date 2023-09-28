5473206230521aed60308a4b178589118dba5397
Yash Raj <yashraj14700728@gmail.com>
2023-09-28 23:29:55 +0530

    Added OnClick Function on Mobile Menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,11 +46,27 @@ const Navbar = () => {
         </div>
         {/*Mobile Menu*/}
         <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-            <li className='py-6 text-4xl'>Home</li>
-            <li className='py-6 text-4xl'>Skills</li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to="home" smooth={true}  duration={500} >
+                    Home
+                </Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to="skills" smooth={true}  duration={500} >
+                    Skills
+                </Link>
+            </li>
             {/* <li className='py-6 text-4xl'>Experience</li> */}
-            <li className='py-6 text-4xl'>Projects</li>
-            <li className='py-6 text-4xl'>About</li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to="projects" smooth={true}  duration={500} >
+                    Projects
+                </Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick}  to="about" smooth={true}  duration={500} >
+                    About
+                </Link>
+            </li>
             
         </ul>
         {/*Social Icons*/}
