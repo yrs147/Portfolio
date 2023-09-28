b6d2699fa974025523722b1707f063aae0a6e0c3
Yash Raj <yashraj14700728@gmail.com>
2023-09-28 08:10:21 +0530

    Added Social Icons and Home Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar';
+import Home from './components/Home';
 import './index.css';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
